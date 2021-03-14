8d7fa5c577ebe9cec3c3c9cd20ac4cee7f5d0cf6
suresh4sparktics <suresh.p@sparktics.com>
2021-03-14 16:30:03 +0530

    shopa page,collection preview ,collection list component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import "./App.css";
-import Homepage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
