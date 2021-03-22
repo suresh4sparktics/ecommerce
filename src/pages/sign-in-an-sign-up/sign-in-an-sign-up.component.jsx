d14652bb6f460eb84f526a9a00163f66f444863b
suresh4sparktics <suresh.p@sparktics.com>
2021-03-23 02:06:43 +0530

    Added SignIn Page

---
+++ src/pages/sign-in-an-sign-up/sign-in-an-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./sign-in-an-sign-up.styles.scss";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-an-sign-up">
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
