5f7e099f5498252a92613ff43754dda1d319b338
suresh4sparktics <suresh.p@sparktics.com>
2021-03-23 12:05:40 +0530

    Changed File Name

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./sign-in-an-sign-up.styles.scss";
+import "./sign-in-and-sign-up.styles.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
 
 const SignInAndSignUpPage = () => (
-  <div className="sign-in-an-sign-up">
+  <div className="sign-in-and-sign-up">
     <SignIn />
   </div>
 );
