6e0d275df5014bd0329b6c0a86e2630e74e04d2d
suresh4sparktics <suresh.p@sparktics@gmail.com>
2021-04-16 19:43:28 +0530

    Added cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import CustomButton from '../custom-button/custom-button.component';
-
-import './cart-dropdown.styles.scss';
+import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
+import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => (
-  <div className='cart-dropdown'>
-    <div className='cart-items' />
+  <div className="cart-dropdown">
+    <div className="cart-items" />
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
